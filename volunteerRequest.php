be20955aa89220a973184b638c312f1933d2e72f
hayamahela <93837679+hayamahela@users.noreply.github.com>
2022-05-13 18:19:49 +0300

    Update volunteerRequest.php

--- volunteerRequest.php
+++ volunteerRequest.php
@@ -11,48 +11,6 @@
   <script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
   <link href="css/new.css" rel="stylesheet">
   <style>
-    /* Remove extra left and right margins, due to padding */
-    .row2 {
-    align-items: center;
-    width: 60%;
-  }
-
-
-    /* Style the counter cards */
-    .card, .row2{
-      box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-      padding: 5px;
-      text-align: left;
-      align-items: center;
-      background-color: #fff;
-      margin: auto;
-    }
-
-    /*form appointment*/
-        .formcontainer input[type=text], formcontainer input[type=checkbox], .formcontainer input[type=datetime-local], select{
-          width: 100%;
-          padding: 15px;
-          margin: 5px 0 22px 0;
-          border: none;
-          background: #f1f1f1;
-        }
-        .formcontainer input[type=text]:focus, .formcontainer input[type=password]:focus {
-          background-color: #ddd;
-          outline: none;
-        }
-
-        /* Set a style for the submit/login button */
-        .formcontainer .btn {
-          background-color: #04AA6D;
-          color: white;
-          padding: 16px 20px;
-          border: none;
-          cursor: pointer;
-          width: 100%;
-          margin-bottom:10px;
-          opacity: 0.8;
-        }
-
 
 .container33 {
   background-color: $primary-color;
@@ -175,53 +133,21 @@
     </div>
   </div>
 
-  <div class="row2">
-    <div class="card">
-      <form action="requestvol.php" method="post" class="formcontainer">
-        <h1>Volunteer Application</h1>
-        <label for=""><b>Full Name</b></label>
-          <input type="text" placeholder="Enter Full Name" name="name" required>
-
-        <label for=""><b>Student ID</b></label>
-        <input type="text" placeholder="Enter Your Student ID" name="ID" required>
-
-        <label for=""><b>Course</b></label>
-        <input type="text" placeholder="Enter A Course Code You'd Like to Teach" name="CourseCode" required>
-
-        <label for=""><b>Email</b></label>
-        <input type="text" placeholder="Enter Your Email For Contact" name="fromEmail" required>
-
-        <label for=""><b>Phone</b></label>
-        <input type="text" placeholder="Enter Your Phone Number" name="phone" required>
-
-        <label for=""><b>Grade</b></label>
-        <input type="text" placeholder="Enter The grade You score in The selected course.." name="grade" required>
-
-        <label for=""><b>Time</b></label>
-        <input type="datetime-local" name="date_time" placeholder="Enter Time" required>
-
-        <button type="submit" name="submit" class="btn" value="submit">Volunteer</button>
-      </form>
-
-    </div>
-  </div>
-
-</div>
  <!-- about section-->
-<article id="aboutus" class="container-fluid bg-grey">
-  <div class="row">
-    <div class="col-sm-4">
-    <a class="navbar-brand" href="#"><img src="css/images/logo.png" height="300px" width="280px"></a>
+    <!-- about section-->
+      <div id="aboutus" class="about-section">
+        <div class="inner-container">
+            <h1>About Us</h1>
+            <p class="text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus velit ducimus, enim inventore earum, eligendi nostrum pariatur necessitatibus eius dicta a voluptates sit deleniti autem error eos totam nisi neque voluptates sit deleniti autem error eos totam nisi neque.
+            </p>
+            <div class="skills">
+                <span>Helpin Sessions</span>
+                <span>Volunteering</span>
+                <span>Offering Workshops</span>
+            </div>
+        </div>
     </div>
-    <div class="col-sm-8">
-      <h2><strong>OUR VALUES</strong></h2><br>
-      <h4><strong>MISSION:</strong> o mission lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4><br>
-      <h4><strong>VISION:</strong> Our vision Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h4>
-    </div>
-  </div>
-</article>
-
 
 <!--footer -->
 <footer>
