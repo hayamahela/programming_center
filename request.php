e71df30c621ea42a65ff895f4ebc2e2edbe00ac6
hayamahela <93837679+hayamahela@users.noreply.github.com>
2022-05-18 01:14:10 +0300

    Update request.php

--- request.php
+++ request.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>NEW</title>
+  <title>Admin</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -151,12 +151,7 @@
        } 
          ?>
          </div></div></div>
-   </div>
-   </div>
-      </div></div></div>
-
- </section>
-</article>
+  
 
 <!--- Workshop Request-->
 <div id="about"  class="about">
@@ -202,17 +197,8 @@
         }   
        } 
          ?>
-         </div></div></div>
-   </div>
-   </div>
-      </div></div></div>
+         </div></div></div></div></div>
 
- </section>
-</article>
-
-
-
-<!--   <div class="containerfooter"></div> -->
 <footer>
   <!-- Footer main -->
   <section class="ft-main">
